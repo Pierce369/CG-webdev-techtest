ebb6262ebc030d1c78a63e4067969c5e267e44b4
pbaker05 <pbaker05@uoguelph.ca>
2022-04-16 12:22:08 -0400

    added express for rest functionality

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-const http = require("http");
-let json = require(__dirname + "/landscapes.json");
+var express = require('express');
+var app = express();
 const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
 
-const requestListener = function (req, res) {
-    switch (req.url){
-        case "/":
-            fs.readFile(__dirname + "/index.html")
-            .then(contents => {
-                res.setHeader("Content-Type", "text/html");
-                res.writeHead(200);
-                res.end(contents);
-            })
-            .catch(err => {
-                res.writeHead(500);
-                res.end(err);
-                return;
-            });
-            break
-        
-        case "/landscapes":
-            res.setHeader("Content-Type", "application/json");
+let json = require(__dirname + "/landscapes.json");
+
+app.get('/', function (req, res) {
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
             res.writeHead(200);
-            res.end(JSON.stringify(json));
-            break
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+    });
+})
 
-        default:
-            res.setHeader("Content-Type", "application/json");
-            res.writeHead(404);
-            res.end(JSON.stringify({error:"Resource not found"}));
-    }
-    
-};
+app.get('/landscapes', function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(json));
+ })
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+
+var server = app.listen(port, function () {
+    console.log("Server is running on http://%s:%s", host, port)
+})
 
